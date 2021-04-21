832b03e4b18e9d1d14cdda1251183f252a4f6cc5
No-025 <77606945+No-025@users.noreply.github.com>
2021-04-21 22:38:31 +0900

    Delete 2745.py

--- BJ/Basic1/Unresolved/2745.py
+++
@@ -1,9 +0,0 @@
-s, b = input().split()
-b = int(b)
-ans = 0
-for ch in s:
-    if '0' <= ch <= '9':
-        ans = ans * b + (ord(ch) - ord('0'))
-    else:
-        ans = ans * b + (ord(ch) - ord('A') + 10)
-print(ans)

578495e1f22e06508f4ad6576916f4044c82a4f5
No-025 <77606945+No-025@users.noreply.github.com>
2021-04-21 22:38:21 +0900

    Delete 1373.py

--- BJ/Basic1/Unresolved/1373.py
+++
@@ -1,9 +0,0 @@
-s = input()
-n = len(s)
-ans = ''
-if n%3 == 1:
-    print(s[0], end='')
-elif n%3 == 2:
-    print((ord(s[0])-ord('0'))*2 + ord(s[1])-ord('0'), end='')
-for i in range(n%3, n, 3):
-    print((ord(s[i])-ord('0'))*4 + (ord(s[i+1])-ord('0'))*2 + (ord(s[i+2])-ord('0')), end='')

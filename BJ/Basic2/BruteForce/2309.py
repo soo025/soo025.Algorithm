1762e9e5506c5982e25def27acf250e2fd7a549a
No-025 <77606945+No-025@users.noreply.github.com>
2021-04-27 08:18:07 +0900

    Update 2309.py

--- BJ/Basic2/BruteForce/2309.py
+++ BJ/Basic2/BruteForce/2309.py
@@ -1,9 +1,11 @@
 n = 9
 a = [int(input()) for _ in range(n)]
+# 오름차순 정렬
 a.sort()
 s = sum(a)
 ans = []
 
+# 9개의 합에서 가짜 난쟁이 2명의 키를 빼주면 100이 나옴
 for i in range(0, n-1):
   for j in range(i+1, n):
     if (s - a[i] - a[j]) == 100:
@@ -12,6 +14,7 @@ for i in range(0, n-1):
   if ans:
     break
     
+# 가짜 난쟁이만 빼고 출력
 for i in range(0, n):
   if i in ans:
     continue
